ffc9da801dc68550fc30d8718eaffb48f1961d9c
Paulo Júnior <paulo.cezar@traefik.io>
2022-02-22 00:19:48 -0300

    Update data.ts to work just sometimes

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,9 +1,8 @@
 export default async function data(req, res) {
   try {
-    // if (Math.random() > 0.5)
-    throw new Error("Can't get data");
+    if (Math.random() > 0.5) throw new Error("Can't get data");
 
-    // res.status(200).json({ worked: true });
+    res.status(200).json({ worked: true });
   } catch (error) {
     res.status(500).json({ message: (error as any).message });
   }
